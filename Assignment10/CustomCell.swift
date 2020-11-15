01dd6a90c19d79d2d51e6becc0d1879e32eb77eb
S-maruyama0223 <wanshanxiangtailang0223@gmail.com>
2020-11-15 16:38:52 +0900

    実装完了

---
+++ Assignment10/CustomCell.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewCell.swift
+//  Assignment10
+//
+//  Created by Shotaro Maruyama on 2020/11/14.
+//  
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+    @IBOutlet weak var prefectureNumber: UILabel!
+    @IBOutlet weak var prefectureName: UILabel!
+}
